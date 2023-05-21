29e2d07f2b589267d22e68f5e879fb663eb17828
Ashik Ali Shanto <shanto902@gmail.com>
2023-05-21 10:24:37 +0600

    categroy added

---
+++ src/components/ToyCard/ToyCard.jsx
@@ -0,0 +1,21 @@
+
+const ToyCard = ({toy}) => {
+    return (
+ 
+<div className="card w-96 bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={toy.pictureURL} alt="action-fig" className="rounded-xl aspect-square object-cover" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">{toy.name}</h2>
+    <p>Price: {toy.price} | Rating: {toy.rating}</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">View Details</button>
+    </div>
+  </div>
+</div>
+
+    );
+};
+
+export default ToyCard;
--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,35 +1,86 @@
 import { useEffect, useState } from "react";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import ToyCard from "../../../components/ToyCard/ToyCard";
 
 const Category = () => {
   const [toys, setToys] = useState([]);
+  const [filteredToys, setFilteredToys] = useState([]);
+  const [selectedSubcategory, setSelectedSubcategory] = useState("");
+  const [tabIndex, setTabIndex] = useState(0);
 
   useEffect(() => {
     fetch("http://localhost:5000/toys")
       .then((res) => res.json())
       .then((data) => setToys(data));
   }, []);
-  return (
-    <div>
-      {toys.map((toy) => (
-        <p key={toy._id}></p>
-      ))}
 
-      <Tabs>
-        <TabList className='flex flex-row items-center w-full justify-center gap-4 text-2xl'>
-          <Tab className=' rounded p-3'>Marvel</Tab>
-          <Tab>DC</Tab>
-          <Tab>One Piece</Tab>
+  useEffect(() => {
+    const filtered = toys.filter(
+      (toy) => toy.subcategory === selectedSubcategory
+    );
+    setFilteredToys(filtered);
+  }, [toys, selectedSubcategory]);
+
+  useEffect(() => {
+    setSelectedSubcategory("Marvel"); // Set the initial selected subcategory here
+  }, []);
+
+  return (
+    <div className="max-w-7xl mx-auto mt-10 ">
+      <Tabs className="mt-10 mb-10" selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList className="flex flex-row items-center w-full justify-center gap-4 text-2xl">
+          <Tab
+            onClick={() => setSelectedSubcategory("Marvel")}
+            className={`rounded p-3 ${tabIndex === 0 ? ' bg-gray-700 text-white' : ''}`}
+          >
+            Marvel
+          </Tab>
+          <Tab
+            onClick={() => setSelectedSubcategory("DC")}
+            className={`rounded p-3 ${tabIndex === 1 ? ' bg-gray-700 text-white' : ''}`}
+          >
+            DC
+          </Tab>
+          <Tab
+            onClick={() => setSelectedSubcategory("One Piece")}
+            className={`rounded p-3 ${tabIndex === 2 ? ' bg-gray-700 text-white' : ''}`}
+          >
+            One Piece
+          </Tab>
         </TabList>
 
-        <TabPanel>
-          <h2>Any content 1</h2>
+        <TabPanel className="flex flex-row justify-center items-center">
+          {filteredToys.length > 0 ? (
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+              {filteredToys.map((toy) => (
+                <ToyCard key={toy._id} toy={toy}></ToyCard>
+              ))}
+            </div>
+          ) : (
+            <h2>No toys found in this category</h2>
+          )}
         </TabPanel>
         <TabPanel>
-          <h2>Any content 2</h2>
+          {filteredToys.length > 0 ? (
+            <div className="grid grid-cols-3 gap-4">
+              {filteredToys.map((toy) => (
+                <ToyCard key={toy._id} toy={toy}></ToyCard>
+              ))}
+            </div>
+          ) : (
+            <h2>No toys found in this category</h2>
+          )}
         </TabPanel>
         <TabPanel>
-          <h2>Any content 3</h2>
+          {filteredToys.length > 0 ? (
+            <div className="grid grid-cols-3 gap-4">
+              {filteredToys.map((toy) => (
+                <ToyCard key={toy._id} toy={toy}></ToyCard>
+              ))}
+            </div>
+          ) : (
+            <h2>No toys found in this category</h2>
+          )}
         </TabPanel>
       </Tabs>
     </div>
